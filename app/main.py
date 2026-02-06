530f46debb29a78461d55cc3575282f585a599ca
Jonathan Jordan <jonathan.d.jordan22@gmail.com>
2026-02-06 15:50:45 -0700

    Add database setup and user model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
+from .database import engine
+from .models import Base
+
 app = FastAPI(title="RelocateReady")
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def health_check():
