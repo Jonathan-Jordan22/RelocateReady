a04a83ceeefb25612040cccaefba4d5858b25ebf
Jonathan Jordan <jonathan.d.jordan22@gmail.com>
2026-02-06 18:29:11 -0700

    Updated main to include score.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .database import engine
 from .models import Base
-from .routes import users, locations
+from .routes import users, locations, score
 
 app = FastAPI(title="RelocateReady")
 
@@ -9,6 +9,7 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(users.router)
 app.include_router(locations.router)
+app.include_router(score.router)
 
 @app.get("/")
 def health_check():
